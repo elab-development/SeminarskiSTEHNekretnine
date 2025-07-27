b06e98e5f0a7ce5ace5bf234ee2b7289e4a25547
DjordjeVujacic4 <djordjevujacic4@gmail.com>
2025-07-28 01:21:26 +0200

    inital laravel application

---
+++ realestate/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InquiryController;
+use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\PropertyTypeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
