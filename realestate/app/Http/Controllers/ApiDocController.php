0e37701c49b8b8b285a7529c94bfa1ccdcac615c
ivaBozsovic <ivaa.bozovic@gmail.com>
2025-07-29 21:18:50 +0200

    api documentation controller

---
+++ realestate/app/Http/Controllers/ApiDocController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *     title="Real Estate API",
+ *     version="1.0.0",
+ *     description="API documentation for the Real Estate Laravel Application",
+ *     @OA\Contact(
+ *         email="support@yourapp.com"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Local Development Server"
+ * )
+ */
+class ApiDocController extends Controller
+{
+    // This class is only for annotations.
+}
